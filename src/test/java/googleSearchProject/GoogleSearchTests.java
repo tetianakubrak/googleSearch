e445369931a3b34bd8c269259df8c1f43b46f8b8
vedo <tetiana.kubrak@partners.tecalliance.net>
2022-08-07 13:07:52 +0200

    Initial commit

---
+++ src/test/java/googleSearchProject/GoogleSearchTests.java
@@ -0,0 +1,16 @@
+package googleSearchProject;
+
+import baseWebSetup.BaseSetup;
+import org.testng.annotations.Test;
+
+
+public class GoogleSearchTests extends BaseSetup {
+
+    @Test()
+    void openBrowser(){
+        System.out.println("Start test");
+    }
+
+
+
+}
